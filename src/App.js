d27b974090f9263c2f540537717c2bf3f3c6ba6f
kdt1802 <khiemha1993@gmail.com>
2022-05-05 13:38:50 +0700

    update text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Hello world! Updated on feature branch...
+          Learn React. Updated on feature branch...
         </a>
       </header>
     </div>
